fa7e8c8296e32c192a214ad271f9ebfafd31e688
ravi <parigha.nawale@gmail.com>
2022-11-05 09:14:55 +0530

    Upadted

--- src/main/java/com/travelbooking/FlightController.java
+++ src/main/java/com/travelbooking/FlightController.java
@@ -4,12 +4,14 @@ import java.util.Date;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.travelbooking.Model.Flight;
 
 @RestController
+@RequestMapping("travelbooking")
 public class FlightController {
 
 	@GetMapping("/myflight")
