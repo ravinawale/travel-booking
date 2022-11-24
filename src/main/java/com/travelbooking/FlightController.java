32e7b7eb52aadc53bbc992e7dd00fb2180cffbde
ravi <parigha.nawale@gmail.com>
2022-11-24 12:43:01 +0530

    Changes

--- src/main/java/com/travelbooking/FlightController.java
+++ src/main/java/com/travelbooking/FlightController.java
@@ -2,6 +2,7 @@ package com.travelbooking;
 
 import java.util.Date;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,6 +18,8 @@ public class FlightController {
 	@GetMapping("/myflight")
 	@ResponseBody
 	public ResponseEntity<Flight> getflight() {
-		return ResponseEntity.ok(new Flight(new Date(),new Date(),"Pune","Delhi"));
+		return ResponseEntity.ok(new Flight(new Date(),new Date(),"Pune","Delhi"))
+							 .status(HttpStatus.OK)
+							 .build();
 	}
 }
